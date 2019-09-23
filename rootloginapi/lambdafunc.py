b1d5df0858209128b208550aeddeab4abf0853d8
anaf3y <47231609+anaf3y@users.noreply.github.com>
2019-09-23 14:35:26 +0100

    Update lambdafunc.py

--- rootloginapi/lambdafunc.py
+++ rootloginapi/lambdafunc.py
@@ -1,24 +1,3 @@
-# Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License is located at
-#
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file.
-# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-# either express or implied. See the License for the specific language governing permissions
-# and limitations under the License.
-#
-# Description: This Lambda function sends an SNS notification to a given AWS SNS topic when an API call event by IAM root user is detected.
-#              The SNS subject is- "API call-<insert call event> by Root user detected in Account-<insert account alias>, see message for further details". 
-#              The JSON message body of the SNS notification contains the full event details.
-# 
-#
-# Author: Sudhanshu Malhotra
-
-
 import json
 import boto3
 import logging
